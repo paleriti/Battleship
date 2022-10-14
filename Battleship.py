54e61c7569cb0e6bb931c2ee3fbad41a5a91a020
paleriti <paleriti@yahoo.com>
2022-10-14 15:50:54 +0300

    Update Battleship.py
    Now ships don't touch each other

--- Battleship.py
+++ Battleship.py
@@ -2,11 +2,14 @@ import random, sys
 
 BOARD_COLOMN_LABELS = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
 BOARD_ROW_LABELS = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+assert len(BOARD_COLOMN_LABELS) == len(BOARD_ROW_LABELS)
+# Don't forget to update BOARD_TEMPLATE if number of colomn or row labels are changed.
 
 EMPTY_SPACE = ' '
+SHIP_MARK = '#'
 
-NUM_OF_SHIPS_ON_BOARD = {'admiral' : 1 }# , 'cruiser' : 2, 'destroyer' : 3, 'submarine' : 4}
-SHIP_LENGTHS = {'admiral' : 5, 'cruiser' : 4, 'destroyer' : 3, 'submarine' : 2}
+NUM_OF_SHIPS_ON_BOARD = {'admiral': 1, 'cruiser': 2, 'destroyer': 3, 'submarine': 4}
+SHIP_LENGTHS = {'admiral': 5, 'cruiser': 4, 'destroyer': 3, 'submarine': 2}
 
 BOARD_TEMPLATE = """
       A B C D E F G H I J
@@ -43,14 +46,14 @@ def boardSetup():
 	deployedShips = {}
 	
 	boardWithoutShips = {}
-	for row in range(10):
-	    for colomn in range(10):
-	        boardWithoutShips[(colomn, row)] = EMPTY_SPACE
+	for row in range(len(BOARD_COLOMN_LABELS)):
+		for colomn in range(len(BOARD_COLOMN_LABELS)):
+			boardWithoutShips[(colomn, row)] = EMPTY_SPACE
 	        
 	boardWithShips = {}
-	for row in range(10):
-	    for colomn in range(10):
-	        boardWithShips[(colomn, row)] = EMPTY_SPACE
+	for row in range(len(BOARD_COLOMN_LABELS)):
+		for colomn in range(len(BOARD_COLOMN_LABELS)):
+			boardWithShips[(colomn, row)] = EMPTY_SPACE
 	        
 	boardWithShips = getNewBoard(boardWithShips, deployedShips)
 	
@@ -64,22 +67,18 @@ def getNewBoard(board, ships_on_board):
 
 	for ship in range(len(shipTypes)):
 		newBoard = deployShip(board, shipTypes[ship], shipNumbers[ship], ships_on_board)
+
+	for y in range(len(BOARD_COLOMN_LABELS)):
+		for x in range(len(BOARD_COLOMN_LABELS)):
+			if newBoard[(x, y)] == "-":
+				newBoard[(x, y)] = EMPTY_SPACE
 	
 	return newBoard
 
 def deployShip(board, shipType, numOfShips, deployedShips):
 	
 	shipLength = SHIP_LENGTHS[shipType]
-	
-	if shipType == 'admiral':
-		shipMark = 'a'
-	elif shipType == 'cruiser':
-		shipMark = 'c'
-	elif shipType == 'destroyer':
-		shipMark = 'd'
-	elif shipType == 'submarine':
-		shipMark = 's'
-	
+		
 	ship = []
 	emptyCells = []
 	
@@ -92,17 +91,13 @@ def deployShip(board, shipType, numOfShips, deployedShips):
 		
 		if shipDirection == 'X':
 			while True:
-				cellX = random.randint(0,10 - shipLength)
-				cellY = random.randint(0,10 - shipLength)
-				
-				#shipCells = []
+				cellX = random.randint(0, len(BOARD_COLOMN_LABELS) - shipLength)
+				cellY = random.randint(0, len(BOARD_COLOMN_LABELS) - shipLength)
 				
 				isStartPointsValid = True
 				for i in range(shipLength):
 					if board[(cellX+i, cellY)] != EMPTY_SPACE:
 						isStartPointsValid = False
-						
-					#shipCells.append((cellX+i,cellY))
 				
 				if isStartPointsValid:
 					startX = cellX
@@ -112,28 +107,24 @@ def deployShip(board, shipType, numOfShips, deployedShips):
 			for shipIndex in range(shipLength):
 				ship.append((startX+shipIndex,startY))
 				
-				emptyCells.append((startX-1,startY))
-				emptyCells.append((startX+shipLength, startY))
-				for i in range(shipLength):
-					emptyCells.append((startX+i, startY-1))
-					emptyCells.append((startX+i, startY+1))
-			
-			print(emptyCells)
+				if startX>=0:
+					emptyCells.append((startX-1,startY))
+					emptyCells.append((startX+shipLength, startY))
+				if startY>=0:
+					for i in range(shipLength+2):
+						emptyCells.append((startX+i-1, startY-1))
+						emptyCells.append((startX+i-1, startY+1))
 			
 			
 		elif shipDirection == 'Y':
 			while True:
-				cellX = random.randint(0,10 - shipLength)
-				cellY = random.randint(0,10 - shipLength)
-			
-				#shipCells = []
+				cellX = random.randint(0, len(BOARD_COLOMN_LABELS) - shipLength)
+				cellY = random.randint(0, len(BOARD_COLOMN_LABELS) - shipLength)
 				
 				isStartPointsValid = True
 				for i in range(shipLength):
 					if board[(cellX, cellY+i)] != EMPTY_SPACE:
 						isStartPointsValid = False
-						
-					#shipCells.append((cellX,cellY+i))
 			
 				if isStartPointsValid:
 					startX = cellX
@@ -143,64 +134,70 @@ def deployShip(board, shipType, numOfShips, deployedShips):
 			for shipIndex in range(shipLength):
 				ship.append((startX,startY+shipIndex))
 				
-		for y in range(10):
-			for x in range(10):
+				if startX>=0:
+					emptyCells.append((startX,startY-1))
+					emptyCells.append((startX, startY+shipLength))
+				if startY>=0:
+					for i in range(shipLength+2):
+						emptyCells.append((startX-1, startY+i-1))
+						emptyCells.append((startX+1, startY+i-1))	
+				
+		for y in range(len(BOARD_COLOMN_LABELS)):
+			for x in range(len(BOARD_COLOMN_LABELS)):
 				if (x, y) in ship:
-					board[(x, y)] = shipMark
-					deployedShips[(x, y)] = shipMark
-					
+					board[(x, y)] = SHIP_MARK
+					deployedShips[(x, y)] = SHIP_MARK
+				if (x, y) in emptyCells:
+					board[(x, y)] = '-'
+				
 	return board
 	
-def getEmptyCellsAroundShips(emptyCells, colomnIndex, rowIndex, lenght):
-	pass
-
-
 def displayBoard(board):
 	gridChars = []
-	for rowIndex in range(10):
-	    for colomnIndex in range(10):
-	        gridChars.append(board[(colomnIndex, rowIndex)])
+	for rowIndex in range(len(BOARD_COLOMN_LABELS)):
+		for colomnIndex in range(len(BOARD_COLOMN_LABELS)):
+			gridChars.append(board[(colomnIndex, rowIndex)])
 	
 	print(BOARD_TEMPLATE.format(*gridChars))
 
 def getPlayerMove(board):
 
-    print("Please select your target or (Q)uit.")
-    while True:  # Keep asking player until they enter a valid move.
-        response = input("> ").upper().strip()
+	print("Please select your target or (Q)uit.")
+	while True:  # Keep asking player until they enter a valid move.
+		response = input("> ").upper().strip()
 
-        if response.startswith("Q"):
-            print("Thanks for playing!")
-            input("Press ENTER to exit.")
-            sys.exit()
+		if response.startswith("Q"):
+			print("Thanks for playing!")
+			input("Press ENTER to exit.")
+			sys.exit()
 
-        if not (2<=len(response)<=3):
-            print("Please use this format when you select your target : J3 or c10")
-            continue  # Ask player again for their move.
+		if not (2<=len(response)<=3):
+			print("Please use this format when you select your target : J3 or c10")
+			continue  # Ask player again for their move.
 	
-        if not response[0] in BOARD_COLOMN_LABELS:
-            print("Your target must start with a valid letter (ie. from A to J)")
-            continue  # Ask player again for their move.
+		if not response[0] in BOARD_COLOMN_LABELS:
+			print("Your target must start with a valid letter (ie. from A to J)")
+			continue  # Ask player again for their move.
             
-        if len(response) == 2:
-	        if not response[1] in BOARD_ROW_LABELS:
-	            print("Your target must end with a valid number (ie. from 1 to 10)")
-	            continue  # Ask player again for their move.
-        elif len(response) == 3:
-        	if not response[1:] == BOARD_ROW_LABELS[-1]:
-	            print("Your target must end with a valid number (ie. from 1 to 10)")
-	            continue  # Ask player again for their move.
+		if len(response) == 2:
+			if not response[1] in BOARD_ROW_LABELS:
+				print("Your target must end with a valid number (ie. from 1 to 10)")
+				continue  # Ask player again for their move.
+		elif len(response) == 3:
+			if not response[1:] == BOARD_ROW_LABELS[-1]:
+				print("Your target must end with a valid number (ie. from 1 to 10)")
+				continue  # Ask player again for their move.
 	
         # Rename colomn and row for easy use.
-        targetRow = int(response[1:]) - 1
-        targetColomn = BOARD_COLOMN_LABELS.index(response[0])
-        target = (targetColomn, targetRow)
+		targetRow = int(response[1:]) - 1
+		targetColomn = BOARD_COLOMN_LABELS.index(response[0])
+		target = (targetColomn, targetRow)
 
-        if board[target] != EMPTY_SPACE:
-            print("Please select an empty cell")
-            continue  # Ask player again for their move.
+		if board[target] != EMPTY_SPACE:
+			print("Your target is already shot!")
+			continue  # Ask player again for their move.
 
-        return target
+		return target
 
 def makePlayerMove(board, target, ships):
 	
